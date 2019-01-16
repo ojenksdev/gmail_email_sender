d9d9940410e521cbed2bcfa0f97802cef9a671a3
Osei J <ojenksdev@gmail.com>
2019-01-15 19:41:35 -0600

    added greater functionality to script and updated Readme file

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 # gmail_email_sender
 Python Script to send emails with attachments via command line. In order to use it simply add whatever attachment you want to send to the same directory as this script. 
 
-***Probably won't work if your gmail account is using additional security(2FA, etc.)***
+NOTES: 
+If using a Mac, make sure you've run the Install Certificates.command file (just search for it with finder).
+If you don't, you could get this SSL error message. Looks like its a bug from Python 3.6 (at least that's what I read on Stackoverflow)
+
+There is an error.txt file that gets created if for some reason connecting to the Gmail server fails. That file will be in the same directory as the script.
+Check that out first to see if the issue is related to gmail or something else. 
+
+***ALSO This Probably won't work if your gmail account is using additional security(2FA, etc.)***
+
+Any issues, feel free to reach out directly at ojenksdev@gmail.com
+
+
+Thanks!
--- email_script.py
+++ email_script.py
@@ -1,6 +1,6 @@
 # Script that sends plain text emails w/ or without attachments from a gmail account.
 
-import email, smtplib, ssl, getpass
+import sys, email, smtplib, ssl, getpass
 
 from email import encoders
 from email.mime.base import MIMEBase
@@ -39,33 +39,43 @@ try:
             part = MIMEBase("application", "octet-stream")
             part.set_payload(attachment.read())
 
-        # Encode file in ASCII to send via email
+            # Encode file in ASCII to send via email
         encoders.encode_base64(part)
 
-        # Add header as key/value pair
+            # Add header as key/value pair
         part.add_header(
             "Content-Disposition",
             f"attachment; filename={filename}",
             )
 
-        # add attachment to message and convert to string
+            # add attachment to message and convert to string
         message.attach(part)
         text = message.as_string()
         print("file attached!")
     else:
         text = body
         print("No filename provided....sending email")                
-except:
-    print("something went wrong")
-    pass
-
+except FileNotFoundError:
+    print("Sorry, I can't find the file. Please check the directory.")
+    send_em_prompt = "\nWould you like to send the email without the file?"
+    send_em_prompt += "\n\nType Y to continue or N to quit\n"
+    keep_running = input(send_em_prompt)
+    if keep_running.upper() == 'Y':
+        print("Trying to send email without attachment....")
+    else:
+        print('Please make sure the file is in the proper location.')
+        sys.exit()
+        
 try: 
-# Create a secure connection
+    # Create a secure connection
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
         server.login(sender_email, password)
         server.sendmail(sender_email, reciever_email, text)
 
     print("Email sent!")
-except:
-    print("Something went wrong.")
+except Exception as e:
+    print("Sorry, We were unable to connect. \nCheck your password and/or Internet connection." +
+          "\nCheck the error.txt file for more information.")
+    with open('error.txt', 'w') as f_obj:
+        f_obj.write(str(e))
